74ec673b361399645afe68c1fc6093c102b6d85d
Sonakshi Singh <sonakshisinghpatel@gmail.com>
2026-07-08 18:27:54 +0530

    Time: 0 ms (100%), Space: 8.9 MB (67.79%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> st;
+        for(int i=0;i<s.size(); i++){
+            if(s[i]=='(' || s[i]=='[' || s[i]=='{'){
+                st.push(s[i]);
+            }
+            else{
+                if(st.empty()) return false;
+            
+                 char x=st.top();
+                 st.pop();
+                if((x=='[' && s[i]==']')||(x=='{' && s[i]=='}')||(x=='(' && s[i]==')')){
+
+                }
+                else return false;
+            
+            }
+        }
+        return st.empty();
+    }
+};

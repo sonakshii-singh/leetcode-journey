52338712a58ee3442197909e64154bc0a62454b5
Sonakshi Singh <sonakshisinghpatel@gmail.com>
2026-07-15 17:46:34 +0530

    Time: 4 ms (49.5%), Space: 118 MB (82.21%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.cpp
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.cpp
@@ -1,29 +1,40 @@
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
- */
 class Solution {
 public:
+    ListNode* reverse(ListNode* head) {
+        ListNode* prev = nullptr;
+        while (head) {
+            ListNode* next = head->next;
+            head->next = prev;
+            prev = head;
+            head = next;
+        }
+        return prev;
+    }
+
     bool isPalindrome(ListNode* head) {
-        stack<int>st;
-        ListNode* temp=head;
-        while(temp){
-            st.push(temp->val);
-            temp=temp->next;
+        if (!head || !head->next) return true;
+
+        // Find middle
+        ListNode* slow = head;
+        ListNode* fast = head;
+
+        while (fast && fast->next) {
+            slow = slow->next;
+            fast = fast->next->next;
         }
-        temp=head;
-        while(!st.empty()){
-            
-          if(st.top()!=temp->val) return false;
-          st.pop();
-          temp=temp->next;
+
+        // Reverse second half
+        ListNode* second = reverse(slow);
+        ListNode* first = head;
+
+        // Compare
+        while (second) {
+            if (first->val != second->val)
+                return false;
+            first = first->next;
+            second = second->next;
         }
+
         return true;
     }
 };

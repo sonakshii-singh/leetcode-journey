828012b67b9db9de60e406a0203503cd70303a46
Sonakshi Singh <sonakshisinghpatel@gmail.com>
2026-07-19 23:55:43 +0530

    Time: 8 ms (5.47%), Space: 22.3 MB (6.47%) - LeetHub

--- 0735-asteroid-collision/0735-asteroid-collision.cpp
+++ 0735-asteroid-collision/0735-asteroid-collision.cpp
@@ -1,35 +1,27 @@
 class Solution {
 public:
-    vector<int> asteroidCollision(vector<int>& asteroids) {
+    vector<int> asteroidCollision(vector<int>& arr) {
         stack<int> st;
 
-        for (int x : asteroids) {
-            bool destroyed = false;
+        for (int i = 0; i < arr.size(); i++) {
 
-            // Collision only when stack top is moving right
-            // and current asteroid is moving left
-            while (!st.empty() && st.top() > 0 && x < 0) {
-                if (st.top() < -x) {
-                    st.pop(); // Stack asteroid explodes
+            if (arr[i] > 0) {
+                st.push(arr[i]);
+            }
+            else {
+                while (!st.empty() && st.top() > 0 && st.top() < abs(arr[i])) {
+                    st.pop();
                 }
-                else if (st.top() == -x) {
-                    st.pop(); // Both explode
-                    destroyed = true;
-                    break;
+                if (!st.empty() && st.top() > 0 && st.top() == abs(arr[i])) {
+                    st.pop();
                 }
-                else {
-                    // Current asteroid explodes
-                    destroyed = true;
-                    break;
+                else if (st.empty() || st.top() < 0) {
+                    st.push(arr[i]);
                 }
             }
-
-            if (!destroyed)
-                st.push(x);
         }
 
         vector<int> ans(st.size());
-
         for (int i = st.size() - 1; i >= 0; i--) {
             ans[i] = st.top();
             st.pop();

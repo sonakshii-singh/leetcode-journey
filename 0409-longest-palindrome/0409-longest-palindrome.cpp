043c79bd37d763ec43e3aee884b893efbb6fab2c
Sonakshi Singh <sonakshisinghpatel@gmail.com>
2026-07-29 21:55:47 +0530

    Time: 3 ms (16.17%), Space: 9.5 MB (5.49%) - LeetHub

---
+++ 0409-longest-palindrome/0409-longest-palindrome.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int longestPalindrome(string s) {
+        unordered_map<char, int> mp;
+
+        for (char c : s)
+            mp[c]++;
+
+        int ans = 0;
+        bool odd = false;
+
+        for (auto it : mp) {
+            if (it.second % 2 == 0)
+                ans += it.second;
+            else {
+                ans += it.second - 1;
+                odd = true;
+            }
+        }
+
+        if (odd) ans++;
+
+        return ans;
+    }
+};

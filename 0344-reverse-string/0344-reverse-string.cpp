2719948992cd9adf8cf90142a004d9caf7753984
Sonakshi Singh <sonakshisinghpatel@gmail.com>
2026-09-01 22:01:32 +0530

    Time: 3 ms (6.45%), Space: 28.1 MB (5.3%) - LeetHub

---
+++ 0344-reverse-string/0344-reverse-string.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    void reverseString(vector<char>& s) {
+        stack<char> st;
+        for(auto it:s){
+          st.push(it);
+        }
+        for(int i=0; i<s.size(); i++){
+            s[i]=st.top();
+            st.pop();
+        }
+    }
+};

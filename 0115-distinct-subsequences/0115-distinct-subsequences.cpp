b59b908e893b448d33d8178879dc07a0edfaae9f
Sonakshi Singh <sonakshisinghpatel@gmail.com>
2026-09-06 23:44:10 +0530

    Time: 26 ms (73.05%), Space: 26.9 MB (64.23%) - LeetHub

--- 0115-distinct-subsequences/0115-distinct-subsequences.cpp
+++ 0115-distinct-subsequences/0115-distinct-subsequences.cpp
@@ -10,8 +10,6 @@ public:
             n + 1,
             vector<int>(m + 1, 0)
         );
-
-        // Empty t can be formed in exactly 1 way
         for (int i = 0; i <= n; i++) {
             dp[i][0] = 1;
         }
@@ -19,10 +17,8 @@ public:
         for (int i = 1; i <= n; i++) {
             for (int j = 1; j <= m; j++) {
 
-                // Do not use s[i-1]
+                
                 dp[i][j] = dp[i - 1][j];
-
-                // Use s[i-1] if it matches t[j-1]
                 if (s[i - 1] == t[j - 1]) {
                     dp[i][j] =
                         (dp[i][j] + dp[i - 1][j - 1]) % MOD;
